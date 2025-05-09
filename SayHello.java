c1e54300a5b90a6832b1b0e9c192158c18289156
maxoto16 <maxoto82@gmail.com>
2025-05-08 23:25:25 -0600

    sayhello

---
+++ SayHello.java
@@ -0,0 +1,7 @@
+public class SayHello{
+  public static void main(String[] args) {
+    System.out.println("HOLA PAPUS");
+    System.out.println("Angel Gilberto Avila Lopez");
+    System.out.println("20");
+  }
+}
